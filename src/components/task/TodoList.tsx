8bd73cdacf5bdc7e5b5ace5334ee6bb12eaa8f7e
bytesbanana <saharat.paynok@gmail.com>
2024-02-24 15:54:57 +0700

    Add no todo message for todolist

--- src/components/task/TodoList.tsx
+++ src/components/task/TodoList.tsx
@@ -5,38 +5,39 @@ import { TodoItem } from "./TodoItem";
 import { Todo } from "../../lib/definition";
 import { useTaskStore } from "../../store/useTaskStore";
 import apis from "../../lib/apis";
+import { useMemo } from "react";
 
 export const TodoList = () => {
-  const { data: todos, isLoading } = useQuery<Todo[]>("todos", apis.getTodos);
+  const { data: todos, isLoading } = useQuery<Todo[]>("todos", () => {
+    return apis.getTodos() || [];
+  });
   const { visibilityFilter } = useTaskStore();
 
-  if (isLoading) {
+  const filteredTodos = useMemo(
+    () =>
+      todos?.filter(({ completed }) => {
+        switch (visibilityFilter) {
+          case "done":
+            return completed;
+          case "undone":
+            return !completed;
+          default:
+            return true;
+        }
+      }) || [],
+    [todos, visibilityFilter]
+  );
+
+  if (isLoading || !todos) {
     return <div>Loading...</div>;
   }
 
   return (
     <div className={styles.todoList}>
-      {todos &&
-        todos?.length > 0 &&
-        todos
-          .filter(({ completed }) => {
-            switch (visibilityFilter) {
-              case "done":
-                return completed;
-              case "undone":
-                return !completed;
-              default:
-                return true;
-            }
-          })
-          .map(({ id, title, completed }) => (
-            <TodoItem
-              key={id}
-              todoId={id}
-              title={title}
-              completed={completed}
-            />
-          ))}
+      {filteredTodos.map(({ id, title, completed }) => (
+        <TodoItem key={id} todoId={id} title={title} completed={completed} />
+      ))}
+      {filteredTodos.length === 0 && <p className={styles.noTaskMsg}>There is no task here.</p>}
     </div>
   );
 };
--- src/lib/apis.ts
+++ src/lib/apis.ts
@@ -3,12 +3,16 @@ import { env } from "./env";
 
 const baseUrl = env.VITE_API_BASE_URL;
 
-async function getTodos() {
+async function getTodos(): Promise<Todo[]> {
   const response = await fetch(`${baseUrl}/todos`);
+  if (!response.ok) {
+    throw new Error("Failed to fetch todos");
+  }
+
   return await response.json();
 }
 
-async function putTodo(data: Partial<Todo>) {
+async function putTodo(data: Partial<Todo>): Promise<Pick<Todo, "id">> {
   const response = await fetch(`${baseUrl}/todos/${data.id}`, {
     method: "PUT",
     body: JSON.stringify(data),
@@ -18,13 +22,21 @@ async function putTodo(data: Partial<Todo>) {
     },
   });
 
+  if (!response.ok) {
+    throw new Error("Failed to update todos");
+  }
+
   return await response.json();
 }
 
-async function deleteTodo(id: number) {
+async function deleteTodo(id: number): Promise<Pick<Todo, "id">> {
   const response = await fetch(`${baseUrl}/todos/${id}`, {
     method: "DELETE",
   });
+
+  if (!response.ok) {
+    throw new Error("Failed to update delete");
+  }
 
   return await response.json();
 }
